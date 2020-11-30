110481071e3caa792fe451301342ee737719c3a7
Cheng Xi Tsou <chengxi600@gmail.com>
2020-11-30 18:16:21 -0500

    stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,35 @@
-let signUp = document.getElementById("sign-up-click");
-signUp.onClick = function() {
-    let sign = document.getElementById("signup");
-    sign.style.visibility = "visible";
-    // make hidding login
-};
+let signupLink = document.getElementById("signup");
+let loginLink = document.getElementById("login");
+
+let login = document.getElementById("login-form");
+let signup = document.getElementById("signup-form");
 
 
-let loginIn = document.getElementById("login-in-click");
-loginIn.onclick = function() {
-    let login = document.getElementById("lognin");
-    login.style.visibility = "visible";
-    // make hidding signup
+function toSignUp() {
+    signupLink.style.display = 'initial';
+    loginLink.style.display = 'none';
+    clearForm(login);
+}
+
+function toLogin() {
+    signupLink.style.display = 'none';
+    loginLink.style.display = 'initial';
+    clearForm(signup);
+}
+
+function clearForm(form) {
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i<inputs.length; i++) {
+        console.log(inputs[i].type);
+        switch (inputs[i].type) {
+            case 'text':
+                inputs[i].value = '';
+                break;
+            case 'password':
+                inputs[i].value = '';
+                break;
+            case 'checkbox':
+                inputs[i].checked = false;
+        }
+    }
 }
